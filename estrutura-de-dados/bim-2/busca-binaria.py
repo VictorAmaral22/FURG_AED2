a427cd9f6b39d42b1374c61ca5f4dfc37cd5e40b
victoramaral22 <victortavamaral@gmail.com>
2024-08-21 20:23:44 -0300

    uai

--- estrutura-de-dados/bim-2/busca-binaria.py
+++ estrutura-de-dados/bim-2/busca-binaria.py
@@ -1,31 +1,26 @@
 lista = [1, 3, 40, 55, 67, 91, 244, 505]
 
 def buscaBinaria (lista, valor, ini=0, fim=len(lista)-1, tam=len(lista)):
-    print(lista, valor, ini, fim, tam)
-    if fim < ini or tam == 0:
+    if tam == 0:
         return -1
-    
-    if lista[tam//2] == valor:
-        return tam//2
     else:
-        # Compara se o valor é maior que o primeiro valor da segunda metade do vetor total
-        if valor > lista[tam//2]:
-            # A metade da direita é do começo até a metade do vetor total
-            newList = lista[slice(tam//2, tam)]
-            pos = buscaBinaria(newList, valor, ini, len(newList)-1, len(newList))
-            print("right", pos)
-            return tam//2 + pos
-        
-        # Compara se o valor é menor que o último valor da primeira metade
-        if valor < lista[tam//2-1]:
-            # A metade da esquerda é do começo até a metade do vetor total
-            newList = lista[slice(ini, tam//2-1)]
-            pos = buscaBinaria(newList, valor, ini, len(newList)-1, len(newList))
-            print("left", pos)
-            return ini + pos
-    
-
-        
-
+        if lista[ini] == valor:
+            return ini
+        elif lista[ini + tam//2] == valor:
+            return ini + tam//2
+        else:
+            if valor > lista[ini + tam//2]:
+                pos = buscaBinaria(lista, valor, ini + tam//2, ini + tam - 1, (ini + tam) - (ini + tam//2))
+                return pos
+            else:
+                pos = buscaBinaria(lista, valor, ini, ini + tam//2 -1, (ini + tam//2 - 1) - ini + 1)
+                return pos
 
 print(buscaBinaria(lista, 1))
+print(buscaBinaria(lista, 3))
+print(buscaBinaria(lista, 40))
+print(buscaBinaria(lista, 55))
+print(buscaBinaria(lista, 67))
+print(buscaBinaria(lista, 91))
+print(buscaBinaria(lista, 244))
+print(buscaBinaria(lista, 505))
