12b9ffa55c8c8543be4dd909d67d4cba084837fb
victoramaral22 <victortavamaral@gmail.com>
2025-01-29 21:26:14 -0300

    tweaks

--- estrutura-de-dados/graph.py
+++ estrutura-de-dados/graph.py
@@ -6,14 +6,19 @@ class Vertex:
 
     def addNeighbor(self, ngb, weight=0):
         self.connectedTo[ngb] = weight
+
     def getConnections(self):
         return self.connectedTo.keys()
+    
     def getId(self):
         return self.id
+    
     def getValue(self):
         return self.value
+    
     def getWeight(self, ngb):
         return self.connectedTo[ngb]
+    
     def __str__(self):
         return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
 
@@ -27,13 +32,16 @@ class Graph:
         newVertex = Vertex(key, value)
         self.vertices[key] = newVertex
         return newVertex
+    
     def getVertex(self, key):
         if self.vertices[key]:
             return self.vertices[key]
         else:
             return None
+        
     def __contains__(self, key):
         return key in self.vertices
+    
     def addEdge(self, f, t, weight=0):
         if f in self.vertices and t in self.vertices:
             fromVertex = self.vertices[f]
@@ -43,14 +51,18 @@ class Graph:
                return None
         else:
             return None
+        
     def getVertices(self):
         return self.vertices.keys()
+    
     def __iter__(self):
         return iter(self.vertices.values())
 
 g = Graph()
+
 for i in range(6):
     g.addVertex(i, "V"+str(i))
+
 g.addEdge(0, 1, 5)
 g.addEdge(0, 5, 2)
 g.addEdge(1, 2, 4)
